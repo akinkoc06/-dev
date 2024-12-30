93f84bfb67082adee0badc0d57299dc3e3c28b12
ahmed akınkoç <95228377+fakekamos@users.noreply.github.com>
2024-12-30 09:39:09 +0300

    Update Program.cs

--- ödev/Program.cs
+++ ödev/Program.cs
@@ -29,30 +29,22 @@
 
                     Console.Write($"{i + 1}. Vize Notu:");
                     double vizenotu = double.Parse(Console.ReadLine());
-                    try
-                    {
-                        vizenotu = double.Parse(Console.ReadLine());
-                    }
-                    catch (Exception a)
-                    {
-                        Console.WriteLine("Sadece 1 ile 100 arası sayıları kullanın.");
+                    if(vizenotu <=0 && vizenotu >= 100)
+{
+    Console.WriteLine("Sadece 0 ile 100 arası sayıları kullanın.");
+}
 
-                    }
 
 
 
 
                     Console.Write($"{i + 1}. Final Notu:");
                     double finalnotu = double.Parse(Console.ReadLine());
-                    try
-                    {
-                        finalnotu = double.Parse(Console.ReadLine());
-                    }
-                    catch (Exception a)
-                    {
-                        Console.WriteLine("Sadece 1 ile 100 arası sayıları kullanın.");
+                   if (finalnotu <= 0 && finalnotu >= 100)
+{
+    Console.WriteLine("Sadece 0 ile 100 arası sayıları kullanın.");
+}
 
-                    }
 
 
                     double ortalama = vizenotu * 0.4 + finalnotu * 0.6;
